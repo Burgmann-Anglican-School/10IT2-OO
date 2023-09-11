2185307b7dd763a5ab4495bdf69f49aeb7047fc4
Edwin Griffin <8305619+EdwinGriffin@users.noreply.github.com>
2023-09-11 15:15:49 +1000

    Lesson 1 stuff
    Created the files and went through the initial stuff for the first lesson.

---
+++ Intro_OO_stuff.py
@@ -0,0 +1,38 @@
+#Simple version
+# Tough to recreate. Could get things wrong. A pain to print everything.
+# nothing stored together
+dog = "Dog"
+dog_breed = "Husky"
+dog_colour = "White & Black"
+# print(dog)
+# print(dog_breed)
+# print(dog_colour)
+
+#Not necessarily bad
+# Things are now grouped together. Easier to print everything. There's no consistency
+#  different dogs could have different things, we could miss things. Tough to make
+#  ones.
+dogs = []
+jimmy = {'name': 'jimmy', 'breed': "husky", "colour": "White & Black"}
+dogs.append(jimmy)
+gerald = {'name': 'gerald', 'breed': "labrador", "colour": "blue"}
+dogs.append(gerald)
+jorj = {'name': 'jorj', 'legs': 8, 'evolution': 'big'}
+dogs.append(jorj)
+for dog in dogs:
+    print(dog['legs'])
+
+#Better method
+class Dog:
+    #dunder method
+    #Initialising the class, also called the class constructor
+    def __init__(self, name, breed, colour, legs):
+        self.name = name
+        self.breed = breed
+        self.colour = colour
+        self.legs = legs
+        
+
+
+
+
